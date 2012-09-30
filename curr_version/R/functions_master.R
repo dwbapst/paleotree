13a227da745db437091167533f6b51973b35b48f
David Bapst <dwbapst@gmail.com>
2012-09-29 21:26:07 -0600

    Change to equation used for simFossilTaxa_SRCond

--- curr_version/R/functions_master.R
+++ curr_version/R/functions_master.R
@@ -2632,7 +2632,7 @@ simFossilTaxa_SRCond<-function(r,avgtaxa,p,q,anag.rate=0,prop.bifurc=0,prop.cryp
 		#mintaxa and maxtaxa will be set within 20% +/- of values necc for given avgtaxa
 			#These should work well if avgtaxa is large (~50 or so)
 	#simFossilTaxa_SRCond(r=0.1,p=0.1,q=0.1,nruns=10,avgtaxa=50,maxExtant=0)
-	N<-avgtaxa/(1-exp(-r/(q+anag.rate+(prop.bifurc*p))))
+	N<-avgtaxa/(r/(r+(q+anag.rate+(prop.bifurc*p))))
 	results<-simFossilTaxa(p=p,q=q,anag.rate=anag.rate,prop.bifurc=prop.bifurc,prop.cryptic=prop.cryptic,nruns=nruns,mintaxa=N,
 			maxtaxa=2*N,maxtime=maxtime,maxExtant=maxExtant,count.cryptic=FALSE,print.runs=print.runs,plot=plot)
 	#if(nruns==1){results<-results[[1]]}

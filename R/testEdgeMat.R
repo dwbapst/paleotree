ab3202fc5fdb03c0a77d569a66d7aa54a8fc59d9
David Bapst <dwbapst@gmail.com>
2015-03-20 08:15:32 -0600

    More minor fixes to testEdgeMat
    c'mon travis please say my build is fixed

--- R/testEdgeMat.R
+++ R/testEdgeMat.R
@@ -20,6 +20,10 @@
 #' tree<-rtree(10)
 #' # should return TRUE
 #' testEdgeMat(tree)
+#'
+#' # should also work on trees with two taxa
+#' testEdgeMat(rtree(2))
+
 
 #' @name testEdgeMat
 #' @rdname testEdgeMat
@@ -29,14 +33,16 @@ testEdgeMat<-function(tree){
 	#test edge matrix
 	if(length(tree$edge[,2])!=length(unique(tree$edge[,2]))){
 		stop("Some nodes are listed as a descendant twice in the edge matrix")}
+	if(Nnode(tree)!=(max(tree$edge[,1])-Ntip(tree))){stop("Number of nodes is incorrect based on edge[,1]?")}
 	if(Ntip(tree)>2){
 		if(Nnode(tree)!=(max(tree$edge[,2])-Ntip(tree))){stop("Number of nodes is incorrect based on edge[,2]?")}
+		#is every internal node listed as a descendant and ancestor, in edge[,2] and edge[,1]?
+		if(!all(sapply((2:Nnode(tree))+Ntip(tree),function(x) any(x==tree$edge[,1])))){
+			stop("Not all internal nodes (except root) listed in edge[,1]?")}
+		if(!all(sapply((2:Nnode(tree))+Ntip(tree),function(x) any(x==tree$edge[,2])))){
+			stop("Not all internal nodes (except root) listed in edge[,2]?")}
+	#}else{
+	#	if(identical(sort(unique(tree$edge[,2])),c(1L,2L))){stop("Number of nodes is incorrect based on edge[,2]?")}
 		}
-	if(Nnode(tree)!=(max(tree$edge[,1])-Ntip(tree))){stop("Number of nodes is incorrect based on edge[,1]?")}
-	#is every internal node listed as a descendant and ancestor, in edge[,2] and edge[,1]?
-	if(!all(sapply((2:Nnode(tree))+Ntip(tree),function(x) any(x==tree$edge[,1])))){
-		stop("Not all internal nodes (except root) listed in edge[,1]?")}
-	if(!all(sapply((2:Nnode(tree))+Ntip(tree),function(x) any(x==tree$edge[,2])))){
-		stop("Not all internal nodes (except root) listed in edge[,2]?")}
 	return(TRUE)
 	}

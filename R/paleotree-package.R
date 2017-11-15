162c57e0feb3782fd07929739cee4ca1354996f3
David Bapst <dwbapst@gmail.com>
2017-11-15 15:03:32 -0600

    added points as import

--- R/paleotree-package.R
+++ R/paleotree-package.R
@@ -90,7 +90,7 @@
 
 #' @importFrom phangorn Descendants Ancestors phyDat ancestral.pars maxCladeCred
 #' @importFrom phytools bind.tip
-#' @importFrom graphics par layout plot hist lines legend polygon title axis
+#' @importFrom graphics par layout plot hist lines legend polygon title axis points
 #' @importFrom grDevices rainbow dev.new
 #' @importFrom methods is
 #' @importFrom utils read.csv type.convert read.table

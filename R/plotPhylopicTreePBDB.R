c919ddb7c35cfa355b6a39d29a47c57766618331
Brian O'Meara <omeara.brian@gmail.com>
2019-02-07 22:52:39 -0500

    now with image caching, still to test

--- R/plotPhylopicTreePBDB.R
+++ R/plotPhylopicTreePBDB.R
@@ -1,5 +1,5 @@
 #' Plot a Phylogeny with Organismal Silhouettes from PhyloPic, Called Via the Paleobiology Database API
-#' 
+#'
 #' This function will take a phylogeny, preferably a taxonomy-tree
 #' created from classification information and/or parent-child taxon
 #' information pulled from the Paleobiology Database via function
@@ -9,7 +9,7 @@
 
 
 #' @details
-#' This function preferably will pull the identifiers for which images are to be associated with the tip taxa from \code{taxaDataPBDB$image_no}. By default, \code{taxaDataPBDB} itself is assumed to be an element of \code{tree} named \code{tree$taxaData}, as the PBDB data table used to construct the tree is appended to the output tree when \code{\link{makePBDBtaxonTree}} is used to construct a taxonomy-tree. If the functions listed in \code{\link{getTaxaDataPBDB}} are used to obtain the taxonomic data, this table will include the \code{image_no} variable, which is the image identifier numbers needed to call PNGs from the Paleobiology Database API. If \code{taxaDataPBDB} isn't provided, either by the user directly, or as an element of \code{tree}. 
+#' This function preferably will pull the identifiers for which images are to be associated with the tip taxa from \code{taxaDataPBDB$image_no}. By default, \code{taxaDataPBDB} itself is assumed to be an element of \code{tree} named \code{tree$taxaData}, as the PBDB data table used to construct the tree is appended to the output tree when \code{\link{makePBDBtaxonTree}} is used to construct a taxonomy-tree. If the functions listed in \code{\link{getTaxaDataPBDB}} are used to obtain the taxonomic data, this table will include the \code{image_no} variable, which is the image identifier numbers needed to call PNGs from the Paleobiology Database API. If \code{taxaDataPBDB} isn't provided, either by the user directly, or as an element of \code{tree}.
 
 
 
@@ -36,7 +36,7 @@
 
 #' @param extraMargin The default is \code{extraMargin = 0.2}.
 
-#' @param rescalePNG If \code{TRUE} (the default), 
+#' @param rescalePNG If \code{TRUE} (the default),
 
 #' @param trimPNG If \code{TRUE} (the default),
 
@@ -48,6 +48,10 @@
 #' is far too much gray-scale, in which case users can try this
 #' option to force all silhouettes to be monochrome.
 #' The default is \code{FALSE}.
+
+#' @param cacheDir If not \code{NULL}, first look here for a cached
+#' version of the images. This makes loading faster.
+#' The default is \code{NULL}.
 
 
 # @param phylopicIDsPBDB ID numbers for images from Phylopic,
@@ -77,36 +81,37 @@
 
 #' @references
 #' Peters, S. E., and M. McClennen. 2015. The Paleobiology Database
-#' application programming interface. \emph{Paleobiology} 42(1):1-7. 
-#' 
+#' application programming interface. \emph{Paleobiology} 42(1):1-7.
+#'
 
 #' @examples
-#' 
+#'
 #' \donttest{
-#' 
-#' 
+#'
+#'
 #' # now plot with phylopic images
-#' plotPhylopicTreePBDB(tree = tree, 
+#' plotPhylopicTreePBDB(tree = tree,
 #' 	taxaDataPBDB = taxaData)
-#' 
+#'
 #' }
-#' 
+#'
 
 
 #' @name plotPhylopicTreePBDB
 #' @rdname plotPhylopicTreePBDB
 #' @export
 plotPhylopicTreePBDB <- function(
-		tree, 
+		tree,
 		taxaDataPBDB = tree$taxaDataPBDB,
-		# phylopicIDsPBDB = NULL, 
+		# phylopicIDsPBDB = NULL,
 		size = 0.9,
 		noiseThreshold = 0.1,
 		extraMargin = 0.2,
 		rescalePNG = TRUE,
 		trimPNG = TRUE,
-		makeMonochrome = FALSE
-		){		
+		makeMonochrome = FALSE,
+		cacheDir = NULL
+		){
 	#########################################
 	# uses calls to the Paleobiology Database's API
 		# to construct a phylogeny with phylopics
@@ -141,17 +146,20 @@ plotPhylopicTreePBDB <- function(
 	# get the plot's own aspect ratio
 	plotAspRatio <- diff(lastPP$x.lim) / diff(lastPP$y.lim)
 	# true aspect ratio is their product apparently
-	plotAspRatio <- plotAspRatio / devAspRatio 
+	plotAspRatio <- plotAspRatio / devAspRatio
 	#
 	# pause 3 seconds so we don't spam the API
-	Sys.sleep(3)
+	if(is.null(cacheDir)) {
+		Sys.sleep(3)
+	}
 	for (i in 1:lastPP$Ntip){
 		# GET IMAGE
-		picPNG <- getPhyloPicPNG(picID = phylopicIDsPBDB[i], 
+		picPNG <- getPhyloPicPNG(picID = phylopicIDsPBDB[i],
 			noiseThreshold = noiseThreshold,
 			rescalePNG = rescalePNG,
 			makeMonochrome = makeMonochrome,
-			trimPNG = trimPNG)
+			trimPNG = trimPNG,
+		  cacheDir = cacheDir)
 		#
 		#########################################
 		#get aspect ratio
@@ -166,7 +174,7 @@ plotPhylopicTreePBDB <- function(
 		}else{
 			# then its skinny, not flat
 			# don't do anything
-			picSize <- size 
+			picSize <- size
 			}
 		###########################################
 		# GET THE COORDINATES
@@ -174,14 +182,14 @@ plotPhylopicTreePBDB <- function(
 		# offset is size/2 by default
 		offset <- size*0.9* plotAspRatio
 		#
-		#points(lastPP$xx,lastPP$yy)	
+		#points(lastPP$xx,lastPP$yy)
 		x<-lastPP$xx[i]+offset
 		y<-lastPP$yy[i]
 		#
 		##################################################
 		#adjust the position of the sides for the image
 		#
-		xAdj <- (picSize/2) * (plotAspRatio/picAspRatio) 
+		xAdj <- (picSize/2) * (plotAspRatio/picAspRatio)
 		yAdj <- picSize /2
 		#
 		##################################################
@@ -198,26 +206,62 @@ plotPhylopicTreePBDB <- function(
 	# what to return? nothing I guess
 	}
 
+#' Cache phylopic images for a tree
+#'
+#' This function takes a tree and saves phylopic thumbnails locally,
+#' in order to dramatically speed up later rendering.
+#'
+
+#' @param tree A phylogeny of class \code{phylo} which will be
+#' plotted, with the terminal tip taxa replaced by silhouettes.
+#' The tree will be plotted with edge lengths.
+
+#' @param taxaDataPBDB  A \code{data.frame} of taxonomic data from
+#' the Paleobiology Database containing an \code{$image_no} variable,
+#' as returned when \code{show = "img"} is used. See \emph{Details}.
+
+#' @param cacheDir Where to save the output PNGs
+
+#' @export
+
+cachePhyloPicPNG <- function(
+	tree, taxaDataPBDB, cacheDir
+){
+	ids <- getPhyloPicIDNum(taxaData=taxaDataPBDB, tree=tree)
+	for(i in seq_along(ids)) {
+		picPNG <- getPhyloPicPNG(ids[i])
+		magick::image_write(picPNG, path=file.path(cacheDir, paste0(ids[i], ".png")), format="png")
+	}
+}
 
 getPhyloPicPNG<-function(
 		picID, noiseThreshold = 0.1,
 		rescalePNG = TRUE, trimPNG = TRUE,
 		makeMonochrome = FALSE,
-		plotComparison = FALSE){
+		plotComparison = FALSE,
+	  cacheDir = NULL){
 	############################################
-	#	
+	#
 	# require(png);require(RCurl)
 	# png::readPNG RCurl::getURLContent
 	#
 	# GET IMAGE
-	# get the URL address for the pic via API
-	apiPicURL <- paste0(
-		"http://paleobiodb.org/data1.2/taxa/thumb.png?id=",
-		picID)
-	# pause 1 second
-	Sys.sleep(runif(1,1,2))
-	# get picPNG		
-	picPNG <-  png::readPNG(RCurl::getURLContent(apiPicURL))
+	#
+	# First try to get a cached version
+	picPNG <- NULL
+	if(!is.null(cacheDir)) {
+		try(picPNG <- png::readPNG(file.path(cacheDir, paste0(picID, ".png"))))
+	}
+	if(is.null(picPNG)) { # we have failed to load cached version
+		# get the URL address for the pic via API
+		apiPicURL <- paste0(
+			"http://paleobiodb.org/data1.2/taxa/thumb.png?id=",
+			picID)
+		# pause 1 second
+		Sys.sleep(runif(1,1,2))
+		# get picPNG
+		picPNG <-  png::readPNG(RCurl::getURLContent(apiPicURL))
+	}
 	############
 	# phylopic PNG is on the fourth slice
 	#  image(picPNG [,,4])
@@ -281,12 +325,12 @@ getPhyloPicIDNum <- function(taxaData, tree){
 	if(is.null(taxaData)){
 		# get image ID numbers using PBDB API calls for each
 			# tip taxon in the tree using the tip labels
-		tiptaxa <- paste0(tree$tip.label, 
+		tiptaxa <- paste0(tree$tip.label,
 			collapse = ",")
 		apiAddressTaxa <- paste0(
 			"http://paleobiodb.org/data1.2/taxa/list.txt?name=",
 			tiptaxa, "&rel=exact&show=img"
-			)	
+			)
 		# call PBDB API
 		tiptaxaData <- read.csv(apiAddressTaxa,
 			stringsAsFactors = FALSE)
@@ -314,7 +358,7 @@ getPhyloPicIDNum <- function(taxaData, tree){
 		# make sure its a character vector
 		if(!is.character(phylopicIDsPBDB)){
 			phylopicIDsPBDB <- as.character(phylopicIDsPBDB)
-			if(!is.character(phylopicIDsPBDB)){		
+			if(!is.character(phylopicIDsPBDB)){
 				stop("Cannot coerce phylopicIDsPBDB to be type character")
 				}
 			}
@@ -330,7 +374,7 @@ getPhyloPicIDNum <- function(taxaData, tree){
 			print(length(names(phylopicIDsPBDB)))
 			print(phylopicIDsPBDB)
 			stop(paste0("If provided, phylopicIDsPBDB must have",
-				" labels matching the tree's tip labels"))	
+				" labels matching the tree's tip labels"))
 			}
 		}
 	# make sure phylopicIDsPBDB is sorted relative to tip order

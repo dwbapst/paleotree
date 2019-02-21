db33f460766209de6dc754e03b10607a9ceaee28
Brian O'Meara <omeara.brian@gmail.com>
2019-02-20 23:43:20 -0500

    tweaking output format of cached PNGs

--- R/plotPhylopicTreePBDB.R
+++ R/plotPhylopicTreePBDB.R
@@ -236,12 +236,12 @@ plotPhylopicTreePBDB <- function(
 #' @export
 
 cachePhyloPicPNG <- function(
-	tree, taxaDataPBDB, cacheDir
+	tree, taxaDataPBDB=tree$taxaDataPBDB, cacheDir
 ){
 	ids <- getPhyloPicIDNum(taxaData=taxaDataPBDB, tree=tree)
 	for(i in seq_along(ids)) {
 		picPNG <- getPhyloPicPNG(ids[i])
-		magick::image_write(picPNG, path=file.path(cacheDir, paste0(ids[i], ".png")), format="png")
+		png::writePNG(picPNG, target=file.path(cacheDir, paste0(ids[i], ".png")))
 	}
 }
 

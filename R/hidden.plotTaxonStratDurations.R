48408c02106fe2835b8e22bd6f876e9366fc2fca
David Bapst <dwbapst@gmail.com>
2019-03-15 13:02:01 -0500

    got boxes to plot

--- R/hidden.plotTaxonStratDurations.R
+++ R/hidden.plotTaxonStratDurations.R
@@ -1,20 +1,24 @@
 
 getSortedMinMaxStratRanges <- function(timeTree,rangesFourDate ){
 	#rangesFourDate <- timeTree$tipTaxonFourDateRanges	
+	#print(rangesFourDate)
 	#########################
 	# get the ranges
 	rangesMinMax <- rangesFourDate[
 		,c("firstapp_max_ma","lastapp_min_ma")
 		]
-	colnames(rangesMinMax) <- c("firstMax","lastMin")
+	rangesMinMax <- as.matrix(rangesMinMax)	
+	rownames(rangesMinMax) <- rownames(rangesFourDate)
 	# transform rangesMinMax for
 	   # same backwards timescale as tree
 	#rangesMinMax <- timeTree$root.time - rangesMinMax
 	rangesMinMaxSorted <- matrix(,Ntip(timeTree),2)
 	for(i in 1:Ntip(timeTree)){
-		whichRanges <- rownames(rangesMinMax) == timeTree$tip.label[i]
+		whichRanges <- rownames(rangesMinMax) == (timeTree$tip.label[i])
+		#print(rangesMinMaxSorted);print(rangesMinMax[whichRanges,])
 		rangesMinMaxSorted[i,] <- rangesMinMax[whichRanges,]
 		}
+	colnames(rangesMinMaxSorted) <- c("firstMax","lastMin")
 	rownames(rangesMinMaxSorted) <- timeTree$tip.label
 	return(rangesMinMaxSorted)
 	}
@@ -29,11 +33,16 @@ plotTaxonStratDurations <- function(
 		){
 	########################
 	#
+	boxCol[is.na(boxCol)] <- "#000000FF"
+	#
 	if(orientation == "rightwards"){	
 		for(i in 1:nrow(rangesMinMax)){
 			yCent <- YY[i]
 			ageMax <- rangesMinMax[i,"firstMax"]
 			ageMin <- rangesMinMax[i,"lastMin"]
+			#print(c(ageMax,ageMin,yCent))
+			#points(ageMax,yCent,cex=2)
+			#
 			graphics::rect(	
 				xleft = ageMax, 
 				xright = ageMin,
